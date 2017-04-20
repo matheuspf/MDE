46cabede2d5812348777cbec8c9ef3d122c01601
matheuspf <matheuspff@gmail.com>
2017-04-20 08:24:33 -0300

    Build skeleton

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "bits/stdc++.h"
 
-#include "VectorBase.h"
+#include "Vector.h"
+#include "MDE.h"
 
 using namespace std;
 
 
 int main()
 {
+    de::Options options;
+
+    options.termination
+
 
     return 0;
 }

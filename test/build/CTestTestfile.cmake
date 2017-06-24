0a676ff5ccca2a9a913b81a506121b75311dbbf3
matheuspf <matheuspff@gmail.com>
2017-06-24 02:37:05 -0300

    Changed Tests

--- test/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matheus/Algoritmos/MDE/test
-# Build directory: /home/matheus/Algoritmos/MDE/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "MDETests")
-subdirs(gtest)
